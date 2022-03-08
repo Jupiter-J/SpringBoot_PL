b87eb9dc12a2e2510154ee1751a98aeb24f09719
Nam_JU <73453283+Jupiter-J@users.noreply.github.com>
2022-03-08 23:54:15 +0900

    Delete Mission3/basic/jpa_CRUD directory

--- Mission3/basic/jpa_CRUD/src/main/java/dev/aquashdw/jpa/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.aquashdw.jpa.repository;
-
-import dev.aquashdw.jpa.entity.BoardEntity;
-import org.springframework.data.repository.CrudRepository;
-                                                        //table 어떤 entity를 위한것이냐 //어떤 타입이냐
-public interface BoardRepository extends CrudRepository<BoardEntity, Long> {
-
-}
--- Mission3/basic/jpa_CRUD/src/main/java/dev/aquashdw/jpa/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.aquashdw.jpa.repository;
-
-import dev.aquashdw.jpa.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-}
